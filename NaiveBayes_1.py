0cb643de55e084ad4efa578f4f9328f3749b4dbd
FAHAD <eng.bokhari@hotmail.com>
2024-05-11 18:31:46 +1200

    Add files via upload
    Update on this var: 
      -  Calculate posterior probability for each class. https://github.com/Heba-aletergi/Naive-Bayes/issues/3
      -  Make prediction on test data. https://github.com/Heba-aletergi/Naive-Bayes/issues/5

--- NaiveBayes_1.py
+++ NaiveBayes_1.py
@@ -16,7 +16,7 @@ class NaiveBayesClassifier:
         num_classes = len(unique_classes)
 
         # Calculate class probabilities
-        self.class_probabilities = self.calculate_class_probabilities(y_train, unique_classes)
+        self.class_probabilities = self.calculate_class_prob(y_train, unique_classes)
 
         # Calculate feature probabilities for each class
         self.feature_probabilities = defaultdict(dict)
@@ -25,29 +25,29 @@ class NaiveBayesClassifier:
             feature_probabilities = defaultdict(dict)
             for feature_index in range(num_features):  # Iterate over each feature
                 feature_values = np.unique(X_train.iloc[:, feature_index])  # get unique val for feature (feature_index)
-                feature_values_counts = self.calculate_feature_counts(subset_X.iloc[:, feature_index], feature_values)
+                feature_values_counts = self.calculate_features_likehood(subset_X.iloc[:, feature_index], feature_values)
                 feature_probabilities[X_train.columns[feature_index]] = feature_values_counts
             self.feature_probabilities[class_label] = feature_probabilities
 
         # Claulate prior for each feature that correspond to class (class_label)
         self.feature_priors = defaultdict(dict)
-        self.feature_priors = self.calculate_prior_features(X_train, num_features)
+        self.feature_priors = self.calculate_features_prior(X_train, num_features)
 
-    def calculate_class_probabilities(self, y_train, unique_classes):
+    def calculate_class_prob(self, y_train, unique_classes):
         class_probabilities = {}
         for class_label in unique_classes:
             class_count = np.sum(y_train == class_label)
             class_probabilities[class_label] = class_count / len(y_train)
         return class_probabilities
 
-    def calculate_feature_counts(self, feature_values, unique_values):
+    def calculate_features_likehood(self, feature_values, unique_values):
         feature_counts = {}
         for value in unique_values:  # Iterate over unique val of feature
             value_count = np.sum(feature_values == value)
             feature_counts[value] = value_count /len(feature_values)
         return feature_counts
 
-    def calculate_prior_features(self, x_train, num_features):
+    def calculate_features_prior(self, x_train, num_features):
         prior_features = defaultdict(dict)
         data_size = len(x_train)
         for feature_index in range(num_features):
@@ -58,16 +58,28 @@ class NaiveBayesClassifier:
 
     def predict(self, X_test):
         predictions = []
-        for instance in X_test:
+        x = np.array(X_test)
+
+        for instance in x:
+            probs_outcome = {}
             likelihoods = []
-            for class_label, class_prob in enumerate(self.class_probabilities):
-                likelihood = class_prob
+            likelihood = 1
+            feature_prior = 1
+            #for class_prob, class_label in enumerate(self.class_probabilities):
+            for class_label, class_prob in self.class_probabilities.items():  # Iterate over Class ()
+                class_prior = class_prob
                 for feature_index, feature_value in enumerate(instance):
-                    feature_probs = self.feature_probabilities[class_label][feature_index]
-                    likelihood *= feature_probs[feature_value]
-                likelihoods.append(likelihood)
-            predicted_class = np.argmax(likelihoods)
-            predictions.append(predicted_class)
+                    likelihood *= self.feature_probabilities[class_label][X_test.columns[feature_index]][feature_value]
+                    feature_prior *= self.feature_priors[X_test.columns[feature_index]][feature_value]
+
+                posterior = (likelihood * class_prior) / (feature_prior)  # Calcualte postirior
+                probs_outcome[class_label] = posterior
+
+            predict_class = max(probs_outcome, key = lambda x: probs_outcome[x]) # compare and get class of hieghts value
+            predictions.append(predict_class)
+            #predicted_class = np.argmax(likelihoods)
+            #predictions.append(predicted_class)
+
         return predictions
 
 def test():
@@ -78,6 +90,14 @@ def test():
     
     clf = NaiveBayesClassifier()
     clf.train(x_train.iloc[:, 1:], y_train.values)
+
+    # Pre-pared test data 
+    data = pd.read_csv('breast-cancer-test.csv')
+    x_test = data.drop(data.columns[1], axis=1)
+    y_test = data[data.columns[1]]
+
+    # Make prediction on test data 
+    predictions = clf.predict(x_train.iloc[:, 1:])
 
 if __name__ == '__main__':
   test()
